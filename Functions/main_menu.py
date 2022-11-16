e7a5c5fca1856555a8f059b57539bfd522ef8e7e
Salocin <salocin.reis@gmail.com>
2022-11-16 18:23:35 -0500

    Created a main menu function and a folder to hold functions we work on

---
+++ Functions/main_menu.py
@@ -0,0 +1,15 @@
+def main_menu():
+    '''Show the main menu options'''
+
+
+    print('\n<Multi> Calculator!\n')
+    print('MENU OPTIONS')
+    print('(1) Basic Calculator')
+    print('(2) Tip Calculator')
+    print('(3) Temperature Converter\n')
+
+
+    
+    
+
+main_menu()
