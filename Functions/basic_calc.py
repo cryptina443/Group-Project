5a3c2acf6db41469553ea5fc238b4ff05ee2295c
Salocin <salocin.reis@gmail.com>
2022-11-16 18:43:21 -0500

    started basic calculator function

---
+++ Functions/basic_calc.py
@@ -0,0 +1,31 @@
+def print_calc_options():
+    '''prints calculator options'''
+
+
+    print('\nOPTIONS\n')
+    print('(add) Addition')
+    print('(sub) Subtraction')
+    print('(mult) Multiplication')
+    print('(div) Division')
+    print('(exp) Exponents\n')
+
+
+def get_choice():
+    '''Get the user's choice'''
+
+    choice = input('Choose an option: ').lower()
+    if choice == 'add':
+        pass
+    elif choice == 'sub':
+        pass
+    elif choice == 'mult':
+        pass
+    elif choice == 'div':
+        pass
+    elif choice == 'exp':
+        pass
+    else:
+        print('Not an option!')
+        get_choice()
+
+get_choice()
