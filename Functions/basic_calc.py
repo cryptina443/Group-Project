8ffaa1731ec1378eb3f19ee12d08ee5f48fb8115
Salocin <salocin.reis@gmail.com>
2022-11-16 19:09:37 -0500

    c:/Users/Rin/Desktop/IntroToPy/groupproject/Group-Project/Functions/basic_calc.py

--- Functions/basic_calc.py
+++ Functions/basic_calc.py
@@ -8,6 +8,8 @@ def print_calc_options():
     print('(mult) Multiplication')
     print('(div) Division')
     print('(exp) Exponents\n')
+    print('(back) Go back to main menu')
+    print('(exit) Exit the program')
 
 
 def get_choice():
@@ -15,7 +17,20 @@ def get_choice():
 
     choice = input('Choose an option: ').lower()
     if choice == 'add':
-        pass
+        nums = []
+        num = input('Enter a number (press enter when done): ')
+        while num != '':
+            if num.isdigit():
+                nums.append(int(num))
+                num = input('Enter a number (press enter when done): ')
+            else:
+                print('Not a number!')
+                num = input('Enter a number (press enter when done): ')
+
+        print(sum(nums))
+        print('\n')
+        get_choice()
+
     elif choice == 'sub':
         pass
     elif choice == 'mult':
@@ -24,6 +39,11 @@ def get_choice():
         pass
     elif choice == 'exp':
         pass
+    elif choice == '':
+        pass
+    elif choice == 'exit':
+        print('Good-bye')
+        exit()
     else:
         print('Not an option!')
         get_choice()
